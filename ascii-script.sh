2b166ab412fd1e588987ffd2737df91efe3a787f
Seyitan Oluwaseitan <146085058+Seyinka@users.noreply.github.com>
2025-02-26 23:40:05 +0100

    added multi jobs

--- ascii-script.sh
+++ ascii-script.sh
@@ -1,14 +1,14 @@
 # Install cowsay utility
-#sudo apt install cowsay -y
+sudo apt install cowsay -y
 
 # Execute cowsay cmd
-#cowsay -f dragon "Run for cover, I am a DRAGON....RAWR" >> dragon.txt
+cowsay -f dragon "Run for cover, I am a DRAGON....RAWR" >> dragon.txt
 
 # Check if file exists           
-#grep -i "dragon" dragon.txt 
+grep -i "dragon" dragon.txt 
 
 # Read repo file
-# cat dragon.txt
+cat dragon.txt
               
 # List repo files        
-#ls -ltra
+ls -ltra
